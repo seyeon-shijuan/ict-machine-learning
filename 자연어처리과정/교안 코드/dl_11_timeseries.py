add461d25d4721a85616a3b4edbc8fad7e2537df
Seyeon <seyeon47@naver.com>
2023-11-05 20:33:27 +0900

    231105

---
+++ 자연어처리과정/교안 코드/dl_11_timeseries.py
@@ -0,0 +1,7 @@
+# https://colab.research.google.com/drive/1HpdtH0NPtThcsIkGCpuPFOEiGMxzNUr1?usp=sharing
+# https://colab.research.google.com/drive/1GsDZcABt8z3skDg1l179980Yj3Oiq8Wz?usp=sharing
+# https://drive.google.com/file/d/1fRNHOsKKjw7SLIXo9R7E9VViGPLc_WZk/view?usp=sharing
+
+print('here')
+
+# https://colab.research.google.com/drive/1gFaMhDI2kUelD01bZ7cLz_KPGS6Otiuq?usp=sharing
